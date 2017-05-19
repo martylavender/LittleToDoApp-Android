687ec091abe0788c12923bab90bafc607b4dbfca
martylavender <martylavender@me.com>
2017-05-20 06:43:28 +0900

    Fixed ability to add items to local storage

--- www/scripts.js
+++ www/scripts.js
@@ -1,2 +1,55 @@
-// This is a JavaScript file
+var todo = {
+    filterFlag: 'all'
+};
 
+document.addEventListener('init', function(event){
+    var view = event.target.id;
+    
+    if(view === 'menu' || view === 'list') {
+        todo[view + 'Init'](event.target);
+    }
+}, false);
+
+todo.listInit = function(target) {
+    this.list = document.querySelector('#todo-list');
+    
+    target.querySelector('#splitter-toggle').addEventListener('click', function() {
+        document.querySelector('#splitter-menu').open();
+    });
+    
+    target.querySelector('#add').addEventListener('click', this.addItemPrompt.bind(this));
+
+    todoStorage.init();
+    this.refresh();
+};
+
+todo.addItemPrompt = function() {
+    ons.notification.prompt('Inset new todo item label.', {
+        title: 'New Item',
+        cancelable: true,
+        
+        callback: function(label) {
+            if(label === '' || label === null) {
+                return;
+            }
+            
+            if(todoStorage.add(label)) {
+                this.refresh();
+            } else {
+                ons.notification.alert('Failed to add item to list');
+            }
+        }.bind(this)
+    });
+}
+
+todo.refresh = function() {
+    var items = todoStorage.filter(this.filterFlag);
+    
+    this.list.innerHTML = items.map(function(item){
+        return document.querySelector('#todo-list-item').innerHTML
+            .replace('{{label}}', item.label)
+            .replace('{{checked}}', item.status === 'completed' ? 'checked' : '');
+    }).join('');
+    
+    
+}
